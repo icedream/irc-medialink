6355227b2e1f01dd283dbef56a7953b738c6cb3e
Carl Kittelberger <icedream@icedream.pw>
2020-07-29 10:26:06 +0200

    Improve code to strip IRC formatting.

--- util.go
+++ util.go
@@ -6,24 +6,26 @@ import (
 )
 
 const (
-	runeIrcBold      = '\x02'
-	runeIrcColor     = '\x03'
-	runeIrcReset     = '\x0f'
-	runeIrcReverse   = '\x16'
-	runeIrcItalic    = '\x1d'
-	runeIrcUnderline = '\x1f'
+	runeIrcBold          = '\x02'
+	runeIrcColor         = '\x03'
+	runeIrcReset         = '\x0f'
+	runeIrcReverse       = '\x16'
+	runeIrcItalic        = '\x1d'
+	runeIrcStrikethrough = '\x1e'
+	runeIrcUnderline     = '\x1f'
 )
 
 var (
-	rxIrcColor = regexp.MustCompile(string(runeIrcColor) + "([0-9]*(,[0-9]*)?)")
+	rxIrcColor = regexp.MustCompile(`[` + strings.Join([]string{
+		string(runeIrcBold),
+		string(runeIrcUnderline),
+		string(runeIrcReset),
+		string(runeIrcReverse),
+		string(runeIrcItalic),
+		string(runeIrcStrikethrough),
+	}, "") + `]|` + string(runeIrcColor) + `(\d\d?(,\d\d?)?)?`)
 )
 
 func stripIrcFormatting(text string) string {
-	text = strings.Replace(text, string(runeIrcBold), "", -1)
-	text = strings.Replace(text, string(runeIrcReset), "", -1)
-	text = strings.Replace(text, string(runeIrcReverse), "", -1)
-	text = strings.Replace(text, string(runeIrcItalic), "", -1)
-	text = strings.Replace(text, string(runeIrcUnderline), "", -1)
-	text = rxIrcColor.ReplaceAllLiteralString(text, "")
-	return text
+	return rxIrcColor.ReplaceAllLiteralString(text, "")
 }
