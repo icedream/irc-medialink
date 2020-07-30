0dc21f0232de87c941e8819f03cacb8fbd0d6905
Carl Kittelberger <icedream@icedream.pw>
2020-07-30 09:03:29 +0200

    Once again fix ldflags.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 go build \
-    -ldflags '-X github.com/icedream/irc-medialink/version.AppVersion='$(git describe --dirty --tags --always)' -X github.com/icedream/irc-medialink/version.appBuildTimestampStr='$(date +%s)' -X github.com/icedream/irc-medialink/version.AppName='"${APPLICATION_NAME:-MediaLink}"' -X github.com/icedream/irc-medialink/version.SupportIRCChannel='"${SUPPORT_IRC_CHANNEL:-"#medialink"}"' '"${EXTRA_LDFLAGS}" \
+    -ldflags "-X \"github.com/icedream/irc-medialink/version.AppVersion=$(git describe --dirty --tags --always)\" -X \"github.com/icedream/irc-medialink/version.appBuildTimestampStr=$(date +%s)\" -X \"github.com/icedream/irc-medialink/version.AppName=${APPLICATION_NAME:-MediaLink}\" -X \"github.com/icedream/irc-medialink/version.SupportIRCChannel=${SUPPORT_IRC_CHANNEL:-"#medialink"}\" ${EXTRA_LDFLAGS}" \
     "$@"
